d1299cfed89f9570543892499274f3f651fc320d
muhammad-junaid1 <mjunaid.swe@gmail.com>
2024-07-25 12:22:26 +0500

    inflows done

--- src/routes/appRoutes/appApi.js
+++ src/routes/appRoutes/appApi.js
@@ -6,7 +6,13 @@ const { hasPermission } = require('@/middlewares/permission');
 const appControllers = require('@/controllers/appControllers');
 const { routesList } = require('@/models/utils');
 
+const { calculate } = require('./extraControllers/inflow');
+
 const routerApp = (entity, controller) => {
+  // Advanced routes
+
+  router.get('/inflow/calculate', calculate);
+
   router
     .route(`/${entity}/create`)
     .post(hasPermission('create'), catchErrors(controller['create']));
